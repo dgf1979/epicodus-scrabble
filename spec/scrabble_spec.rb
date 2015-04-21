245351b775ac9e2f4a4d82673ba59d792c51c4ec
Guest User <student@epicodus.com>
2015-04-21 11:25:39 -0700

    code reviewed and approved

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -8,12 +8,12 @@ describe("scrabble") do
       expect(('a').scrabble()).to(eq(1))
   end
 
-  it('returns a scrabble score for a letter') do
+  it('returns a scrabble score for a word') do
     expect(('aaaaazzzzz').scrabble()).to(eq(55))
   end
 
 
-  it('returns a scrabble score for a letter') do
+  it('returns a scrabble score for a word with a capital letter') do
     expect(('Aaaaazzzzz').scrabble()).to(eq(55))
   end
 end
