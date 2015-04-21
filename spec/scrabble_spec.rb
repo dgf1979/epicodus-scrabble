10bd106257c0a1b41df6f677ad74056120ae692a
Guest User <student@epicodus.com>
2015-04-21 10:38:07 -0700

    initial spec test

---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+require "pry"
+require "scrabble"
+
+
+describe("scrabble") do
+    it('returns a scrabble score for a letter') do
+      expect('a'.scrabble()).to(eq(1))
+  end
+end
