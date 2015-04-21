b62225ad7e9960abb5464794ae4d6552688a31de
Guest User <student@epicodus.com>
2015-04-21 11:06:36 -0700

    added all letters and scores associtated with said letters

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -7,4 +7,8 @@ describe("scrabble") do
     it('returns a scrabble score for a letter') do
       expect(('a').scrabble()).to(eq(1))
   end
+
+  it('returns a scrabble score for a letter') do
+    expect(('aaaaazzzzz').scrabble()).to(eq(55))
+  end
 end
